dae7af51fb1dd61fbd940a787f97ff2250b18462
vityaman <vityaman.dev@yandex.ru>
2023-03-08 19:39:58 +0300

    [math:linal:vector] clang-tidy

--- ci/project/lint.sh
+++ ci/project/lint.sh
@@ -9,11 +9,4 @@ clang-format \
     --files=build/sources.txt
 
 echo 'Linting...'
-clang-tidy \
-    --checks=*,-llvmlibc-callee-namespace,-llvmlibc-restrict-system-libc-headers,-llvmlibc-implementation-in-namespace,-fuchsia-trailing-return,-altera-unroll-loops,-cppcoreguidelines-pro-bounds-constant-array-index,-fuchsia-default-arguments-calls,-modernize-use-trailing-return-type,-google-readability-namespace-comments,-llvm-namespace-comment,-fuchsia-overloaded-operator,-google-readability-todo,-llvm-header-guard,-llvm-include-order,-cppcoreguidelines-special-member-functions,-hicpp-special-member-functions \
-    --header-filter=.* \
-    --format-style=.clang-format \
-    --use-color \
-    --warnings-as-errors=* \
-    -p ./compile_commands.json \
-    src/*.cpp test/*.cpp
+clang-tidy -header-filter=.* -p ./compile_commands.json src/*.cpp test/*.cpp
