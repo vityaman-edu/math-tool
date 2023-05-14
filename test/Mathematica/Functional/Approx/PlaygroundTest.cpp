e2140642044078cf6ec4b4846d5243b7c09db363
Victor Smirnov <vityaman.dev@yandex.ru>
2023-05-14 16:28:05 +0300

    [doc] added report

--- test/Mathematica/Functional/Approx/PlaygroundTest.cpp
+++ test/Mathematica/Functional/Approx/PlaygroundTest.cpp
@@ -4,6 +4,7 @@
 #include "Mathematica/Functional/Exploration/Tabulate.hpp"
 #include "Mathematica/Interop/Polynomial.hpp"
 #include "Mathematica/Statistics/RSS.hpp"
+#include "Mathematica/Statistics/Score.hpp"
 #include "Symatica/Expression/DSL/Literals.hpp"
 #include <gtest/gtest.h>
 
@@ -39,9 +40,14 @@ TEST(Approx, Playground) { // NOLINT
   const auto lineS = Statistics::standartDeviation(line, points);
   const auto parabolaS = Statistics::standartDeviation(parabola, points);
 
+  const auto lineR2 = Statistics::Score::R2(line, points);
+  const auto parabolaR2 = Statistics::Score::R2(parabola, points);
+
   std::cout << "Line: " + sline->asString() << std::endl;
   std::cout << "Line: STD = " + lineS.asString() << std::endl;
+  std::cout << "Line: R2 = " + lineR2.asString() << std::endl;
 
   std::cout << "Parabola: " + sparabola->asString() << std::endl;
   std::cout << "Parabola: STD = " + parabolaS.asString() << std::endl;
+  std::cout << "Parabola: R2 = " + parabolaR2.asString() << std::endl;
 }
